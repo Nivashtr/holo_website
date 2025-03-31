daaa513e34da265621f3d1a72333309102a69e8d
riyaz <s.mohamedriyaz93@gmail.com>
2025-03-31 22:20:59 +0530

    after adding small page solution

--- public/script.js
+++ public/script.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menuToggle');
-    const mobileMenu = document.getElementById('mobileMenu');
-    const closeMenu = document.getElementById('closeMenu');
-    
-    // Open menu
-    menuToggle.addEventListener('click', function() {
-      mobileMenu.classList.add('active');
-      document.body.style.overflow = 'hidden'; // Prevent background scrolling
-    });
-    
-    // Close menu
-    closeMenu.addEventListener('click', function() {
-      mobileMenu.classList.remove('active');
-      document.body.style.overflow = ''; // Restore scrolling
-    });
-  });
-
   //ABHD PAGE SCRIPT
   //scroll number count
 function animateCounter(element, target, duration) {
